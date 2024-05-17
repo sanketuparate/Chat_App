dcfe0932ed760db6b9987d366daad2149ac44454
sanket uparate <uparatesanket@gmail.com>
2024-05-17 20:26:33 +0530

    Initialized commit

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -25,8 +25,9 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			default: "",
 		},
-	},//createdAt , updatedAt => Member since <CreatedAt>
-	{timestamps:true}
+		// createdAt, updatedAt => Member since <createdAt>
+	},
+	{ timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
